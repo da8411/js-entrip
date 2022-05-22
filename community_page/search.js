7077bf9964a1cd8bdf5ff2dad3d50a6e25ad4f89
dajin jung <dajin@blueprint-lab.com>
2022-05-22 21:30:27 +0900

    Dev search function

---
+++ community_page/search.js
@@ -0,0 +1,9 @@
+jQuery(function($) { 
+    $('#filterText').keyup(function(event) { 
+      var val = $(this).val(); if (val == "") { 
+        $('.board-item').show(); 
+      } else { 
+        $('.board-item').hide(); $(".board-item:contains('"+val+"')").show(); 
+      } 
+    });
+  });
